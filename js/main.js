ab60aab363690ea108e47b56a9eb8bab0efa5ea6
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-07-07 01:15:40 +0400

    skills section ends

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//////////skills Tab starts////////
+const tabs= document.querySelectorAll(['data-target']),
+tabContent = document.querySelectorAll(['data-content'])
+ 
+tabs.forEach(tab => {
+    tab.addEventListener("click",() =>{
+ const target = document.querySelector(tab.dataset.target)
+tabContent.forEach( tabContents => {
+tabContents.classList.remove('skills__active')
+})
+target.classList.add('skills__active')
+tabs.forEach( tab =>{
+tab.classList.remove('skills__acive')
+
+})
+tab.classList.add()
+
+    })
+
+})
