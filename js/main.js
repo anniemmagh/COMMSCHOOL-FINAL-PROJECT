9f1a15c7787392cb32c323157ead38e5ff772903
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-07-07 21:06:39 +0400

    Mixitup js

--- js/main.js
+++ js/main.js
@@ -18,3 +18,20 @@ tab.classList.add()
     })
 
 })
+
+// /mixitup filter js///////
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+/////////links active work//////
+const linkWork=document.querySelectorAll('.work__item')
+function activeWork() {
+    linkWork.forEach(L=>I.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+linkWork.forEach(L=>I.addEventListener("click",activeWork))
